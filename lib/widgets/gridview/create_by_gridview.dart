b44763251e01074d8a2a5c2be4cd8c99f75c6570
wujinli <wujinli>
2020-09-18 10:16:32 +0800

    优化gridview组件创建事例

--- lib/widgets/gridview/create_by_gridview.dart
+++ lib/widgets/gridview/create_by_gridview.dart
@@ -4,51 +4,154 @@ import 'package:flutter_demo/customwidget/decs_text_widget.dart';
 import 'package:flutter_demo/customwidget/grid_item_widget.dart';
 
 class CreateByGridView extends StatefulWidget {
+  CreateType createType;
+  Axis axis;
+
+  CreateByGridView({@required this.createType, this.axis});
+
   @override
-  _CreateByGridViewState createState() => _CreateByGridViewState();
+  _CreateByGridViewState createState() =>
+      _CreateByGridViewState(createType: createType,axis: axis);
 }
 
 class _CreateByGridViewState extends State<CreateByGridView> {
-  Axis _axi = Axis.vertical;
+  CreateType createType;
+  Axis axis;
+
+  _CreateByGridViewState({@required this.createType, this.axis});
+
+  _initWidget() {
+    Widget initWidget;
+    switch (createType) {
+      case CreateType.none:
+        initWidget = GridView(
+          scrollDirection: axis,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 2, mainAxisSpacing: 4),
+          children: <Widget>[
+            GridViewItem(color: Colors.primaries[1]),
+            GridViewItem(color: Colors.primaries[2]),
+            GridViewItem(color: Colors.primaries[3]),
+            GridViewItem(color: Colors.primaries[4]),
+            GridViewItem(color: Colors.primaries[5]),
+            GridViewItem(color: Colors.primaries[6]),
+            GridViewItem(color: Colors.primaries[1]),
+            GridViewItem(color: Colors.primaries[2]),
+            GridViewItem(color: Colors.primaries[3]),
+            GridViewItem(color: Colors.primaries[4]),
+            GridViewItem(color: Colors.primaries[5]),
+            GridViewItem(color: Colors.primaries[6]),
+            GridViewItem(color: Colors.primaries[1]),
+            GridViewItem(color: Colors.primaries[2]),
+            GridViewItem(color: Colors.primaries[3]),
+            GridViewItem(color: Colors.primaries[4]),
+            GridViewItem(color: Colors.primaries[5]),
+            GridViewItem(color: Colors.primaries[6]),
+          ],
+        );
+        break;
+      case CreateType.builder:
+        initWidget = GridView.builder(
+            itemCount: 100,
+            scrollDirection: axis,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, mainAxisSpacing: 2, crossAxisSpacing: 4),
+            itemBuilder: (context, index) {
+              return GridViewItem(
+                  color: Colors.primaries[index % Colors.primaries.length]);
+            });
+        break;
+      case CreateType.count:
+        initWidget = GridView.count(
+          scrollDirection: axis,
+          crossAxisCount: 3,
+          children: List.generate(100, (index) {
+            return Container(
+              height: 80,
+              color: Colors.primaries[index % Colors.primaries.length],
+            );
+          }),
+        );
+        break;
+      case CreateType.custom:
+        initWidget = GridView.custom(
+          scrollDirection: axis,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          childrenDelegate: SliverChildBuilderDelegate((context, index) {
+            return Container(
+              height: 80,
+              color: Colors.primaries[index % Colors.primaries.length],
+            );
+          }, childCount: 100),
+        );
+        break;
+      case CreateType.extent:
+        initWidget = GridView.extent(
+          scrollDirection: axis,
+          maxCrossAxisExtent: 10,
+          children: List.generate(100, (index) {
+            return Container(
+              color: Colors.primaries[index % Colors.primaries.length],
+            );
+          }),
+        );
+        break;
+      default:
+        initWidget = GridView(
+          scrollDirection: axis,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 2, mainAxisSpacing: 4),
+          children: <Widget>[
+            GridViewItem(color: Colors.primaries[1]),
+            GridViewItem(color: Colors.primaries[2]),
+            GridViewItem(color: Colors.primaries[3]),
+            GridViewItem(color: Colors.primaries[4]),
+            GridViewItem(color: Colors.primaries[5]),
+            GridViewItem(color: Colors.primaries[6]),
+            GridViewItem(color: Colors.primaries[1]),
+            GridViewItem(color: Colors.primaries[2]),
+            GridViewItem(color: Colors.primaries[3]),
+            GridViewItem(color: Colors.primaries[4]),
+            GridViewItem(color: Colors.primaries[5]),
+            GridViewItem(color: Colors.primaries[6]),
+            GridViewItem(color: Colors.primaries[1]),
+            GridViewItem(color: Colors.primaries[2]),
+            GridViewItem(color: Colors.primaries[3]),
+            GridViewItem(color: Colors.primaries[4]),
+            GridViewItem(color: Colors.primaries[5]),
+            GridViewItem(color: Colors.primaries[6]),
+          ],
+        );
+        break;
+    }
+    return initWidget;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (axis == null) {
+      axis = Axis.vertical;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return PageBaseContainScaffoldWidget(
       title: 'GridView',
-      child: SingleChildScrollView(
-        scrollDirection: _axi,
-        reverse: false,
-        child: Column(
-          children: <Widget>[
-            DescTextWidget(
-                content:
-                    'gridDelegate参数控制子控件的排列，有2个选择\nSliverGridDelegateWithFixedCrossAxisCount：交叉轴方向上固定数量，对于垂直方向的GridView来说交叉轴方向指的是水平方向\nSliverGridDelegateWithMaxCrossAxisExtent：交叉轴方向上尽量大，比如水平方上有500空间，指定此值为150，那么可以放3个，剩余一些空间，此时GridView将会缩小每一个Item，放置4个。'),
-            Container(
-              width: double.infinity,
-              height: 400,
-              child: GridView(
-                scrollDirection: _axi,
-                shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3, crossAxisSpacing: 5, mainAxisSpacing: 5),
-                children: <Widget>[
-                  GridViewItem(color: Colors.primaries[0]),
-                  GridViewItem(color: Colors.primaries[1]),
-                  GridViewItem(color: Colors.primaries[2]),
-                  GridViewItem(color: Colors.primaries[3]),
-                  GridViewItem(color: Colors.primaries[4]),
-                  GridViewItem(color: Colors.primaries[5]),
-                  GridViewItem(color: Colors.primaries[6]),
-                  GridViewItem(color: Colors.primaries[7]),
-                ],
-              ),
-            ),
-            DescTextWidget(
-                content:
-                    'SliverGridDelegateWithFixedCrossAxisCount有属性介绍如下：\ncrossAxisCount：交叉轴方向上个数。\nmainAxisSpacing：主轴方向上2行之间的间隔。\ncrossAxisSpacing：交叉轴方向上之间的间隔。\nchildAspectRatio：子控件宽高比。'),
-          ],
-        ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            'createType:$createType,\nscrollDirection:$axis',
+            style: TextStyle(color: Colors.blue),
+          ),
+          Expanded(child: _initWidget()),
+        ],
       ),
     );
   }
 }
+
+enum CreateType { none, builder, custom, count, extent }
--- lib/widgets/gridview/grdiview_widget.dart
+++ lib/widgets/gridview/grdiview_widget.dart
@@ -3,25 +3,133 @@ import 'package:flutter_demo/customwidget/width_match_button_widget.dart';
 import 'package:flutter_demo/customwidget/page_base_widget.dart';
 import 'package:flutter_demo/route_animaton/page_route_animation.dart';
 import 'package:flutter_demo/widgets/gridview/create_by_gridview.dart';
-import 'package:flutter_demo/widgets/gridview/gridview_scroll.dart';
+import 'package:flutter_demo/customwidget/decs_text_widget.dart';
 
-class GridViewWidget extends StatelessWidget {
+class GridViewWidget extends StatefulWidget {
+  @override
+  _GridViewWidgetState createState() => _GridViewWidgetState();
+}
+
+class _GridViewWidgetState extends State<GridViewWidget> {
+  CreateType createType;
+  Axis axis = Axis.vertical;
+
+  _createType() {
+    return Wrap(
+      alignment: WrapAlignment.spaceEvenly,
+      children: <Widget>[
+        RaisedButton(
+          color: Colors.lightGreenAccent,
+          child: Text(
+            'none',
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+          onPressed: () {
+            setState(() {
+              createType = CreateType.none;
+            });
+          },
+        ),
+        RaisedButton(
+          color: Colors.lightGreenAccent,
+          child: Text(
+            'builder',
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+          onPressed: () {
+            setState(() {
+              createType = CreateType.builder;
+            });
+          },
+        ),
+        RaisedButton(
+          color: Colors.lightGreenAccent,
+          child: Text(
+            'custom',
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+          onPressed: () {
+            setState(() {
+              createType = CreateType.custom;
+            });
+          },
+        ),
+        RaisedButton(
+          color: Colors.lightGreenAccent,
+          child: Text(
+            'count',
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+          onPressed: () {
+            setState(() {
+              createType = CreateType.count;
+            });
+          },
+        ),
+        RaisedButton(
+          color: Colors.lightGreenAccent,
+          child: Text(
+            'extent',
+            style: TextStyle(color: Colors.white, fontSize: 18.0),
+          ),
+          onPressed: () {
+            setState(() {
+              createType = CreateType.extent;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  _initScrollDirection() {
+    return Column(
+      children: <Widget>[
+        WidthMatchBottonWidget(
+            title: 'vertical',
+            onPressed: () {
+              setState(() {
+                axis = Axis.vertical;
+              });
+            }),
+        WidthMatchBottonWidget(
+            title: 'horizontal',
+            onPressed: () {
+              setState(() {
+                axis = Axis.horizontal;
+              });
+            })
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return PageBaseContainScaffoldWidget(
         title: 'gridview',
         child: Column(
           children: <Widget>[
+            DescTextWidget(content: 'gridview创建方式：'),
+            _createType(),
+            DescTextWidget(content: 'gridview滑动方向：'),
+            _initScrollDirection(),
+            DescTextWidget(content: 'gridview创建查看：'),
+            Text(
+              '已选：\ncreateType:$createType,\nscrollDirection:$axis',
+              style: TextStyle(color: Colors.blue),
+            ),
             WidthMatchBottonWidget(
                 title: 'GridView',
                 onPressed: () {
-                  Navigator.of(context)
-                      .push(PageRouteAnimation(widget: CreateByGridView()));
-                }),WidthMatchBottonWidget(
-                title: 'scrollDirection',
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(PageRouteAnimation(widget: GridViewScrollDirection()));
+                  Navigator.of(context).push(
+                    PageRouteAnimation(
+                      widget: CreateByGridView(
+                        createType: createType,
+                        axis: axis,
+                      ),
+                      animationType: AnimationType.SCALETRANSITIONTYPE,
+                    ),
+                  );
                 }),
           ],
         ));
